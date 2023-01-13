e002050a7e2c329ee55bf3f62df29ab3ff26fd47
felixokolo <pheelix.info@gmail.com>
2023-01-13 19:48:26 +0100

    correction

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,16 @@
 -- Buy buy
 
-DELIMITER $$;
-CREATE TRIGGER reduce
+DELIMITER $$
+CREATE TRIGGER IF NOT EXISTS reduce
 AFTER INSERT
 ON orders FOR EACH ROW
 BEGIN
-DECLARE old_quant INT(11);
+DECLARE old_quant INT;
 SELECT quantity
 INTO old_quant
 FROM items
 WHERE NEW.item_name = items.name;
-UPDATE items SET quantity = @old_quant - NEW.number
+UPDATE items SET quantity = (old_quant - NEW.number)
 WHERE NEW.item_name = items.name;
 END;$$
+DELIMITER ; $$
