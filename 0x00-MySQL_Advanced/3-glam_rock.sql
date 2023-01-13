6d44f386b763b923e5f263446afd07856eca4345
felixokolo <pheelix.info@gmail.com>
2023-01-13 13:21:14 +0100

    addition

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- Old school band 
-
-SELECT band_name, (split-formed) AS lifespan
+-- lists all bands with Glam rock as their
+-- main style, ranked by their longevity
+SELECT band_name AS band_name, (split-formed) AS lifespan
 FROM metal_bands
 WHERE style='Glam rock'
 ORDER BY lifespan DESC;
