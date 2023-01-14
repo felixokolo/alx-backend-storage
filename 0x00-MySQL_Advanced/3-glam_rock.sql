9c3e71111a52fb517dd2fd76040ec8c571b831a9
felixokolo <pheelix.info@gmail.com>
2023-01-14 10:35:04 +0100

    correction

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
--- Old school band 
--- lists all bands with Glam rock as their
-SELECT band_name AS band_name,
-IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+
+--  lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE 'Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
