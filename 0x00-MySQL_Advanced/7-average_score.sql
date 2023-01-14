32edce08aafebf30dae3ad3f09769d6238deb3fe
felixokolo <pheelix.info@gmail.com>
2023-01-14 19:49:26 +0100

    addtion

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,13 @@
+-- Buy buy
+
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id_ INT)
+BEGIN
+DECLARE avg_sc INT;
+SELECT AVG(score)
+INTO avg_sc
+FROM corrections
+WHERE corrections.user_id = user_id_;
+UPDATE users SET average_score = avg_sc;
+END;$$
+DELIMITER ; $$
